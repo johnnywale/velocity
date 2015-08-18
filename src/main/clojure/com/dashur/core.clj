41fcc0f32f14d5d3c5fe52d05ab341f97dcb3da1
johnnywale <johnnywalee@gmail.com>
2015-08-18 23:26:24 +0800

    add project template

--- src/main/clojure/com/dashur/core.clj
+++ src/main/clojure/com/dashur/core.clj
@@ -1,67 +1,33 @@
-(ns  com.dashur.core
+(ns com.dashur.core
   (:gen-class)
-  (:use com.dashur.velocity)
-   (:import [org.apache.velocity.app VelocityEngine]
-           [org.apache.velocity.runtime RuntimeConstants]
-           [org.apache.velocity.runtime.resource.loader ClasspathResourceLoader]
-           [org.apache.velocity VelocityContext Template]
-           [java.io StringWriter]))
+  (:use com.dashur.velocity))
 
-
-(defprotocol TestMethod (getTest [this] ))
-(defrecord Person  [fname lname address]  TestMethod  (  getTest [this] "Implementation"   ) )
+(defprotocol TestMethod (getTest [this]))
+(defrecord Person [fname lname address] TestMethod (getTest [this] "Implementation"))
 (defrecord Address [street city state zip])
-(defrecord dto-template [  ^Class dtoClazz ^Class updateModel  ] )
-
-
-(defn dto-name [^dto-template  dto] (.getSimpleName (:dtoClazz dto)))
-(defn contrcoller-name [ ^dto-template dto ](str (dto-name dto) "Controller"))
-(defn service-name [ ^dto-template dto ](str  (dto-name dto) "Service"))
-(defn update-model [ ^dto-template dto ](if (nil?(:updateModel dto)) (dto-name dto ) (.getSimpleName(:updateModel dto))))
-(def model-url [^dto-template dto]   .to com.google.common.base.CaseFormat.LOWER_CAMEL    )
-
-(defn- ^{:tag VelocityContext} ->context [kvs] ; private VelocityContent
-  "Convert a vector of key/value pairs to a velocity context instance"
-  (let [^VelocityContext ctx (VelocityContext.)]
-    (loop [[k v & r] kvs]
-      (.put ctx (name k) v)
-      (when r
-        (recur r)))
-    ctx))
+(defrecord dto-template [^Class dtoClazz ^Class updateModel])
+(defn dto-name [^dto-template dto] (.getSimpleName (:dtoClazz dto)))
+(defn contrcoller-name [^dto-template dto] (str (dto-name dto) "Controller"))
+(defn service-name [^dto-template dto] (str (dto-name dto) "Service"))
+(defn update-model [^dto-template dto] (if (nil? (:updateModel dto)) (dto-name dto) (.getSimpleName (:updateModel dto))))
+(defn model-url [^dto-template dto] (.to com.google.common.base.CaseFormat/LOWER_CAMEL com.google.common.base.CaseFormat/LOWER_HYPHEN (dto-name dto)))
+(defn dao-name [^dto-template dto] (str (dto-name dto) "Dao"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (def v (prepare-context))
-  (def ^Template template (.getTemplate v "hello.vm"))
-  (def kvs [:name "name" :key "abcdef"  ])
-  (let [^VelocityContext ctx (->context kvs) ^StringWriter sw (StringWriter.)]
-  (.merge template ctx sw)
-    (println (.toString sw))
-  )
-  (def stu (Person. "Stu" "Halloway"
+  (def stu (Person. "StuLLL" "Halloway"
              (Address. "200 N Mangum"
                "Durham"
                "NC"
                27701)))
-;  (println  contrcollerName          )
-;  (println  (:lname stu))
-;  (println  (getTest stu))
-  (def mo (dto-template.   com.dashur.code_template.User nil ))
-  (println(:dtoClazz  mo))
-  (println (nil?(:updateModel mo)))
-  (println (dto-name mo ) )
-  (println   (update-model  mo))
-  (println  (update-model  (dto-template.   com.dashur.code_template.User com.dashur.code_template.UserUpdateModel )))
-
-
-
-
-
-
-
-
-
-
-;;  (println  (update-model  (dto-template.   com.dashur.code_template.User nil )))
+  (def kvs [:name "name" :person stu])
+  (def env (com.dashur.velocity.CodeEnveriment. "hello.vm" "generated/Hello.txt")) ;
+  (write-with-template env kvs)
+  (def mo (dto-template. com.dashur.code_template.User nil))
+  (println (:dtoClazz mo))
+  (println (nil? (:updateModel mo)))
+  (println (dto-name mo))
+  (println (update-model mo))
+  (println (update-model (dto-template. com.dashur.code_template.User com.dashur.code_template.UserUpdateModel)))
   )
--- src/main/clojure/com/dashur/velocity.clj
+++ src/main/clojure/com/dashur/velocity.clj
@@ -1,16 +1,53 @@
 (ns com.dashur.velocity
-  (:gen-class)
+  (:gen-class :methods)
   (:import [org.apache.velocity.app VelocityEngine]
            [org.apache.velocity.runtime RuntimeConstants]
            [org.apache.velocity.runtime.resource.loader ClasspathResourceLoader]
            [org.apache.velocity VelocityContext Template]
-           [java.io StringWriter]))
+           [java.io StringWriter FileWriter]
+           [clojure.lang Keyword]))
+
+(defn global-singleton [f]
+  (let [instance (atom nil)
+        make-instance (fn [_] (f))]
+    (fn [] (or @instance (swap! instance make-instance)))))
+(defrecord  CodeEnveriment [template-name file-name])
+(defn- ^{:tag VelocityEngine}  new-velocity-engine []
+  (let [^VelocityEngine v (VelocityEngine.)]
+    (.setProperty v org.apache.velocity.runtime.RuntimeConstants/RESOURCE_LOADER "classpath") ;
+    (.setProperty v "classpath.resource.loader.class" "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader")
+    (.init v)
+    v)
+  )
 
 
-(defn prepare-context []
-  (let [ velocity (VelocityEngine.)]
-  (.setProperty velocity org.apache.velocity.runtime.RuntimeConstants/RESOURCE_LOADER "classpath") ;
-  (.setProperty velocity "classpath.resource.loader.class" "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader")
-  (.init velocity)
-       velocity))
+(def ^{:dynamic true :private true :constant true} single-engine  (new-velocity-engine))
 
+(defn ^{:tag VelocityContext}  context [kvs] ; private VelocityContent
+  "Convert a vector of key/value pairs to a velocity context instance"
+  (let [^VelocityContext ctx (VelocityContext.)]
+    (loop [[k v & r] kvs]
+      (.put ctx (name k) v)
+      (when r
+        (recur r)
+        )
+      )
+    (.put ctx "key" Keyword)
+    ctx
+    )
+  )
+
+
+
+(defn write-with-template[  ^CodeEnveriment env,   kvs   ]
+  (let  [^Template template (.getTemplate   single-engine    (:template-name env))]
+    (let [^VelocityContext ctx ( context kvs) ^StringWriter sw (StringWriter.)]
+      (.merge template ctx sw)
+      (println (str sw))
+      (let [file2Write (clojure.java.io/writer (:file-name env))]
+        (.merge template ctx file2Write)
+        (.flush file2Write)
+        )
+      )
+    )
+  )
